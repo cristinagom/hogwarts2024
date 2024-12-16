4d7a42763ac7818701841bb7f0b6ed96912730c7
cristinagom <gomezalonso.cristina@gmail.com>
2024-12-16 14:59:37 +0100

    Consultas native rectificadas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -36,10 +36,10 @@ public class Main {
          *  Dentro de la clase Person indicar:
          *
          @NamedQuery(name="Person_MasPuntos",query="select p  from Person as p where p.id =" +
-         "(select h2.personByReceiver.id  from HousePoints h2 group by h2.personByReceiver.id " +
-         "having  sum(h2.points) >= all (select sum(h.points) from HousePoints h group by h.personByReceiver.id))")
+         "(select h2.receiver.id  from HousePoints h2 group by h2.receiver.id " +
+         "having  sum(h2.points) >= all (select sum(h.points) from HousePoints h group by h.receiver.id))")
          */
-        //Query2 : select p from Person as p where p.id =(select h2.personByReceiver.id  from HousePoints h2 group by h2.personByReceiver.id having  sum(h2.points) >= all (select sum(h.points) from HousePoints h group by h.personByReceiver.id))
+        //Query2 : select p from Person as p where p.id =(select h2.receiver.id  from HousePoints h2 group by h2.receiver.id having  sum(h2.points) >= all (select sum(h.points) from HousePoints h group by h.receiver.id))
 
         Query q2 = em.createNamedQuery("Person_MasPuntos", Person.class);
         Person p = (Person) q2.getSingleResult();
@@ -50,18 +50,105 @@ public class Main {
          *  3. Inventa consultas SQL (nativeQuery) que cumplan los siguientes requisitos:
          *  3.1. Una consulta con un innerjoin -- Ojo! No se puede hacer por culpa de que no coinciden los ids y se tienen que igualar manualmente. Sino, siempre con ON
          *      Consulta propuesta: listado de todas las casas con el total de ocupantes
-         *  Query31: select h.name, count(p.id) from Person as p , House as h where p.houseByHouseId=h.id   group by h.id
+         *  Query31: select h.name, count(p.id) from Person as p , House as h where p.house.id=h.id   group by h.id
          */
 
 
         Query consulta = em.createNativeQuery("select h.name, count(p.id) from person as p inner"+
-                " join house as h  where p.house_id=h.id   group by h.id", Tuple.class);
+                " join house as h where p.house_id=h.id   group by h.id", Tuple.class);
         List<Tuple> tuplas = consulta.getResultList();
 
         System.out.println("Nombre de casa:"+"  Numero habitantes:");
         tuplas.forEach(tuple -> {
             System.out.println(tuple.get(0, String.class)+" ******* "+tuple.get(1,Long.class));
         });
+
+        /*****
+         *  3. Inventa consultas SQL (nativeQuery) que cumplan los siguientes requisitos:
+         *  3.2. Una consulta con un outerjoin
+         *      Consulta propuesta: listar el nombre de los cursos con el nombre de su profesor
+         * Query32 (NATIVE): select  c.name,p.first_name  from course as c left join  person as p on c.teacher_id=p.id;
+         */
+
+        Query consulta3 = em.createNativeQuery("select  c.name,p.first_name  from course as c" +
+                " left join  person as p on c.teacher_id=p.id;", Tuple.class);
+        List<Tuple> tuplas2 = consulta3.getResultList();
+
+        System.out.println("Nombre del curso:"+"  Nombre del profesor:");
+        tuplas2.forEach(tuple -> {
+
+            System.out.println(tuple.get(0, String.class)+" ******* "+tuple.get(1, String.class));
+
+        });
+
+
+        /*****
+         *  3. Inventa consultas SQL (nativeQuery) que cumplan los siguientes requisitos:
+         *  3.3. Una consulta con una subconsulta (que no se puede codificar con un JOIN en la consulta externa)
+         *      Consulta propuesta: listar las casas con la cantidad de diferentes cursos en los que se encuentran
+         *      implicados mediante los alumnos
+         *      Consulta SQL :  select h.name,
+         *                      ( select count(e.person_enrollment) from enrollment as e
+         * 	                        where e.person_enrollment in (select p.id from person as p
+         * 							where p.house_id=h.id)) as cantidad_cursos
+         * 								from house as h;
+         *
+         *
+         */
+
+        Query consulta4 = em.createNativeQuery("select \n" +
+                "h.name,\n" +
+                "(select count(e.person_enrollment) from enrollment as e\n" +
+                "where e.person_enrollment in (select p.id from person as p \n" +
+                "where p.house_id=h.id)) as cantidad_cursos\n" +
+                "from house as h", Tuple.class);
+        List<Tuple> tuplas3 = consulta4.getResultList();
+
+        System.out.println("Nombre del curso:"+"  Cantidad de cursos implicados:");
+        tuplas3.forEach(tuple -> {
+
+            System.out.println(tuple.get(0, String.class)+" ******* "+tuple.get(1, Long.class));
+
+        });
+
+
+        /*****
+         *  EXTRA1: Realiza el borrado de un personaje concreto a través de una Query.
+         *
+         */
+
+        Query delte = em.createNativeQuery("delete from person as p where  p.first_name like 'Euan'");
+        //int resultado = delte.executeUpdate();
+        //System.out.println("Resultado de la operación de borrado: "+ resultado);
+
+
+        /*****
+         *   EXTRA2: Busca ejemplos de la notación HQL y demuestra su uso en la base de datos hogwarts
+         *
+         *
+         *
+         */
+
+
+        Query qry5 = em.createQuery( "select c.teacher.firstName from Course c", Tuple.class);
+
+        List<Tuple> list4 = qry5.getResultList();
+
+        list4.forEach(list1 -> {
+            System.out.println(list1.get(0, String.class));
+
+        });
+
+        Query qry6 = em.createQuery( "select p.firstName, p.house.name from Person p where p.id=:id", Tuple.class);
+        qry6.setParameter("id",12);
+
+        List<Tuple> list6 = qry6.getResultList();
+
+        list6.forEach(list2 -> {
+            System.out.println(list2.get(0, String.class)+" ******* "+ list2.get(1,String.class));
+
+        });
+
 
 
         em.close();
